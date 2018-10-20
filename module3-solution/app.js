24f9b08ea09d518534df96790a2647b920e6a493
amnaredo <amnaredo@gmail.com>
2018-10-20 19:35:37 +0200

    update

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -17,26 +17,37 @@ function MenuSearchResultDirective()
 	bindToController: true,
 	scope: {
 	  found: '<',
-      onRemove: '@'
+      onRemove: '&'
 	},
   };
   return ddo;
-};
+}
 
 
 NarrowItDownController.$inject = ['$scope', 'MenuSearchService'];
 function NarrowItDownController($scope, MenuSearchService) {
   var controller = this;
 
-  controller.found = [];
+  //controller.found = [];
   
   controller.NarrowItDown = function () {
-    console.log("searchTerm = " + $scope.searchTerm);
-    controller.found = MenuSearchService.getMatchedMenuItems($scope.searchTerm);
+    //console.log("searchTerm = " + $scope.searchTerm);
+	
+    MenuSearchService.getMatchedMenuItems($scope.searchTerm).then( function (result){
+	  controller.found = result;
+	  //console.log ("Found "  + controller.found.length + " items: " + controller.found);
+	});
+	
   };
 
   controller.onRemove = function (index) {
+    //console.log ("llamada a onRemove");
     controller.found.splice(index, 1);
+  };
+  
+  controller.notFound = function () {
+    return controller.found && controller.found.length == 0;
+	//console.log(controller.found.length + " AAAA");
   };
 }
 
@@ -51,37 +62,37 @@ function MenuSearchService($http) {
 	  url: "https://davids-restaurant.herokuapp.com/menu_items.json"
 	});
     
-	return promise.then(function (result) 
+	return promise.then(function (response) 
 	{
 	  // process result and only keep items that match
       var foundItems = [];
-	  var allItems = result.data;
+	  
+	  if (searchTerm && searchTerm.length > 0)
+	  {
+	  var allItems = response.data.menu_items;
 	  var i = 0;
 	  for (i=0; i<allItems.length; i++)
 	  {
 	  
 	      var menuItem = allItems[i];
           var menuItemDescription = menuItem.description;
-		  var foundTerm = (menuItemDescription.indexOf(searchTerm) >= 0);
+		  var foundTerm = menuItemDescription.indexOf(searchTerm) >= 0;
 		  
 		  if (foundTerm)
 		  {
+		    //console.log(menuItem);
 		    foundItems.push(menuItem);
 		  }
 	  }
-
+	  }
+      //console.log ("Found "  + foundItems.length + "  items: " + foundItems);
       // return processed items
       return foundItems;
-	
 	});
 	
   
   };
  
 }
-
-
-
-
 
 })();
