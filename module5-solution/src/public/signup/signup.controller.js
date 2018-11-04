eccbe974cc19d40aa97bb4d131b60844b26ee55f
amnaredo <amnaredo@gmail.com>
2018-11-04 22:22:59 +0100

    work in progress...

---
+++ module5-solution/src/public/signup/signup.controller.js
@@ -0,0 +1,18 @@
+(function () {
+
+angular.module('public')
+.controller('SignUpFormController', SignUpFormController);
+
+SignUpFormController.$inject = ['signedUp', 'SignUpService'];
+function SignUpFormController(signedUp, SignUpService) {
+  var reg = this;
+  reg.signedUp = signedUp;
+
+
+  reg.submit = function () {
+    reg.signedUp = SignUpService.RegisterUser(reg.user);
+    // reg.completed = true;
+  };
+}
+
+})();
---
+++ module5-solution/src/public/signup/signup.service.js
@@ -0,0 +1,61 @@
+(function () {
+"use strict";
+
+angular.module('public')
+.service('SignUpService', SignUpService);
+
+
+SignUpService.$inject = ['$q', '$http', 'ApiPath'];
+function SignUpService($q, $http, ApiPath) {
+  var service = this;
+
+
+  service.userInfo = {};
+  service.signedUp = false;
+
+  service.checkSignUp = function () {
+    var deferred = $q.defer();
+    deferred.resolve(service.signedUp);
+    // deferred.resolve(true);
+
+    return deferred.promise;
+  };
+
+  service.RegisterUser = function (user)
+  {
+    var menuItem  = user.menuitem;
+    return $http({
+      url: ApiPath + "/menu_items/" + menuItem + ".json"
+    }).then(function (response){
+      service.userInfo = user;
+      service.userInfo.menuItemData = response.data;
+      service.signedUp = true;
+      return  true;
+    }, function (response){
+      return false;
+    });
+  };
+  //
+  // service.getCategories = function () {
+  //   return $http.get(ApiPath + '/categories.json').then(function (response) {
+  //     return response.data;
+  //   });
+  // };
+  //
+  //
+  // service.getMenuItems = function (category) {
+  //   var config = {};
+  //   if (category) {
+  //     config.params = {'category': category};
+  //   }
+  //
+  //   return $http.get(ApiPath + '/menu_items.json', config).then(function (response) {
+  //     return response.data;
+  //   });
+  // };
+
+}
+
+
+
+})();
